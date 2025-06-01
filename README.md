95c0bbb7a625be5b5cfab3f1750ff6ba6caa661b
Azekowka <gabjtov@gmail.com>
2025-06-01 17:09:31 +0500

    updated setup

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-step-1: pip install fastapi uvicorn
+step-0: python -m venv venv, .\venv\Scripts\activate
+step-1: pip install fastapi uvicorn or pip install fastapi[standard] 
 step-2: create app.py file
 step-3: uvicorn main:app --reload
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/", summary="Root endpoint", tags=["General documentation"])
 def read_root():
     return {"message": "Hello, FastAPI!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, reload=True)
